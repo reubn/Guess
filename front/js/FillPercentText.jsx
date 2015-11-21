8ee4c9ae4cc7f2f8f8883448a0918d9a17d86dd0
Reuben <reuben.eggar@me.com>
2015-11-21 09:52:01 +0000

    Clean

--- front/js/FillPercentText.jsx
+++ front/js/FillPercentText.jsx
@@ -5,7 +5,6 @@ module.exports = React.createClass({
   displayName: "FillPercentText",
   render: function(){
     const colour = this.props.colourStops[Object.keys(this.props.colourStops).sort((a, b) => a - b).find(s => this.props.percent <= s )]
-    console.log(colour, this.props.percent, this)
     return <span className={styles("text")}
           style={{
             background: `linear-gradient(${this.props.direction}, ${colour} 0%,${colour} ${this.props.percent}%,rgb(232, 232, 232) ${this.props.percent}%,rgb(232, 232, 232) 100%)`,
